a8e8e38222741c0f66416b61a88818d380ea947d
utkuonursahin <sahinutkuonur@gmail.com>
2021-11-29 23:51:36 +0300

    style update 0.1

--- src/components/Header/Cart/Cart.js
+++ src/components/Header/Cart/Cart.js
@@ -1,14 +1,17 @@
 import iconDelete from "../../../images/icon-delete.svg";
 import firstThumbnail from "../../../images/image-product-1-thumbnail.jpg";
 import {useBasket} from "../../../context/BasketContext";
+import {useCart} from "../../../context/CartContext";
 const Cart = () => {
   const {basket,setBasket} = useBasket()
+  const {isCartClicked} = useCart()
   return (
-      <div className="cart">
+      <div className={`cart ${!isCartClicked && 'hidden'}`}>
         <header className="cart__header">
           <h2 className="cart__header--heading">Cart</h2>
-          <img src={iconDelete} alt="Delete icon" className="cart__header--icon" onClick={() => setBasket(null)}/>
+          <img src={iconDelete} alt="delete icon" className="cart__header--icon" onClick={() => setBasket(null)}/>
         </header>
+        {!basket && <div className="cart__body--empty">"Your basket is empty"</div>}
         {basket &&
         <div className="cart__body">
           <img src={firstThumbnail} alt="Sneakers fall edition" className="cart__body--img"/>
@@ -20,11 +23,9 @@ const Cart = () => {
             </div>
           </div>
           <button className="cart__body--btn">Checkout</button>
-        </div>
-        }
-        {!basket && <div className="cart__body--empty">"Your basket is empty"</div>}
+        </div>}
       </div>
   );
-};
+}
 
 export default Cart;
